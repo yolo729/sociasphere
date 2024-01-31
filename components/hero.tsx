572204be3678ba01087149940dbcaab8d39997d4
Artem Milevskyi <milevskyi.a@gmail.com>
2024-01-30 21:25:44 -0800

    Finish structure of Hero Section Page

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,61 +1,99 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
+import Image from "next/image";
+import Link from "next/link";
+import PentrackImg from "@/public/images/pentrack.png";
+import GroupImg from "@/public/images/group.png";
+
+import NewsIcon from "@/public/svgs/news.svg";
+import ArrowIcon from "@/public/svgs/arrow.svg";
+import HightlightIcon from "@/public/svgs/highlight.svg";
 
 export default function Hero() {
   return (
     <section className="relative">
-
       {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
-        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
-              <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#illustration-01)" fillRule="evenodd">
-            <circle cx="1232" cy="128" r="128" />
-            <circle cx="155" cy="443" r="64" />
-          </g>
-        </svg>
-      </div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
+      <div className="mx-auto px-20 sm:px-24">
         {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
+        <div className="pt-52 pb-12 md:pt-56 md:pb-20">
           {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
-            <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+          <div className="grid grid-cols-3 gap-14">
+            <div className="col-span-2">
+              <p className="mt-12 text-7xl font-normal">
+                Your Social Media Sidekick
+              </p>
+              <div className="flex gap-4">
+                <div className="flex flex-col">
+                  <Image src={PentrackImg} alt="pen track" />
+                  <p className="mt-12">
+                    With the power of AI, generate social media captions, posts,
+                    blogs, and newsletters in{" "}
+                    <span className="font-bold ml-1">seconds</span>.
+                  </p>
+                  <Link
+                    href="/home"
+                    className="btn-sm mt-12 text-gray-200 bg-indigo-500 rounded-xl hover:bg-indigo-400 w-fit "
+                  >
+                    <span>Try Free Trial</span>
+                  </Link>
                 </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                <Image src={GroupImg} alt="group img" className="" />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div className="flex flex-col mb-4">
+                <Image
+                  src={NewsIcon}
+                  alt="news svg"
+                  width={67}
+                  height={67}
+                  className="ml-[50%]"
+                />
+                <p className="text-3xl">Sentiment Analysis</p>
+                <p className="text-base">
+                  Discover trends and understand your customers with our
+                  <span className="font-bold ml-1">
+                    Sentiment Analysis Software.
+                  </span>
+                </p>
+                <Link href="#">Learn more</Link>
+              </div>
+              <div className="flex flex-col my-4">
+                <div className="flex">
+                  <p className="text-3xl">AI-Generated Captions</p>
+                  <Image
+                    src={ArrowIcon}
+                    alt="arrow svg"
+                    width={105}
+                    height={99}
+                    className="-mt-16"
+                  />
                 </div>
+                <p className="text-base">
+                  Generate relevant, unique captions based off{" "}
+                  <span className="font-bold ml-1">your</span> photos.
+                </p>
+                <Link href="#">Learn more</Link>
+              </div>
+              <div className="flex flex-col my-4">
+                <p className="text-3xl">Blog Posts & Newsletters</p>
+                <div className="flex">
+                  <p className="text-base">
+                    Quickly create blog posts and newsletters with a click by
+                    simply providing a photo and description.
+                  </p>
+                  <Image
+                    src={HightlightIcon}
+                    alt="highlight svg"
+                    width={126}
+                    height={75}
+                  />
+                </div>
+                <Link href="#">Learn more</Link>
               </div>
             </div>
           </div>
-
-          {/* Hero image */}
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={768}
-            thumbHeight={432}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} />
-
         </div>
-
       </div>
     </section>
-  )
+  );
 }
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,60 +1,88 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import Link from 'next/link'
-import Logo from './logo'
-import Dropdown from '@/components/utils/dropdown'
-import MobileMenu from './mobile-menu'
+import Link from "next/link";
+import Logo from "./logo";
+import Dropdown from "@/components/utils/dropdown";
+import MobileMenu from "./mobile-menu";
 
 export default function Header() {
-
-  const [top, setTop] = useState<boolean>(true)
+  const [top, setTop] = useState<boolean>(true);
 
   // detect whether user has scrolled the page down by 10px
   const scrollHandler = () => {
-    window.pageYOffset > 10 ? setTop(false) : setTop(true)
-  }  
+    window.pageYOffset > 10 ? setTop(false) : setTop(true);
+  };
 
   useEffect(() => {
-    scrollHandler()
-    window.addEventListener('scroll', scrollHandler)
-    return () => window.removeEventListener('scroll', scrollHandler)
-  }, [top])
+    scrollHandler();
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
 
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? 'bg-white backdrop-blur-sm shadow-lg' : ''}`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+    <header
+      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
+        !top ? "bg-white backdrop-blur-sm shadow-lg" : ""
+      }`}
+    >
+      <div className="mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
-
           {/* Site branding */}
-          <div className="shrink-0 mr-4">
+          <div className="flex-1 mr-4">
             <Logo />
           </div>
 
+          {/* Site Links */}
+          <div className="shrink-0 inline-flex ">
+            <Link href="/product" className="px-4 font-medium">
+              Product
+            </Link>
+            <Link href="/pricing" className="px-4 font-medium">
+              Pricing
+            </Link>
+            <Link href="/contact" className="px-4 font-medium">
+              Contact
+            </Link>
+          </div>
+
           {/* Desktop navigation */}
-          <nav className="hidden md:flex md:grow">
+          <nav className="flex-1 hidden md:flex md:grow">
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                <Link
+                  href="/signin"
+                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Login
+                </Link>
               </li>
               <li>
-                <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Sign up</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>
+                <Link
+                  href="/signup"
+                  className="rounded-lg btn-sm text-gray-200 bg-indigo-500 hover:bg-indigo-400 ml-3"
+                >
+                  <span>Sign Up</span>
+                  {/* <svg
+                    className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1"
+                    viewBox="0 0 12 12"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                      fillRule="nonzero"
+                    />
+                  </svg> */}
                 </Link>
               </li>
             </ul>
-
           </nav>
 
           <MobileMenu />
-
         </div>
       </div>
     </header>
-  )
+  );
 }
